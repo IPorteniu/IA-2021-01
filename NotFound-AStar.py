b9dc5f962bd12ecf5364859caa74b8171d6b081c
Shiohne <Shiohne@github.com>
2021-04-02 21:37:59 -0500

    A* ex
    A* example for review

--- NotFound-AStar.py
+++ NotFound-AStar.py
@@ -25,7 +25,52 @@ class Edge(object):
     def __init__(self, pointB, distance):
         self.pointB = pointB
         self.distance = distance
-
+        
+def aStar(start, goal, grid):
+    #The open and closed sets
+    openset = set()
+    closedset = set()
+    #Current point is the starting point
+    current = start
+    #Add the starting point to the open set
+    openset.add(current)
+    #While the open set is not empty
+    while openset:
+        #Find the item in the open set with the lowest G + H score
+        current = min(openset, key=lambda o:o.G + o.H)
+        #If it is the item we want, retrace the path and return it
+        if current == goal:
+            path = []
+            while current.parent:
+                path.append(current)
+                current = current.parent
+            path.append(current)
+            return path[::-1]
+        #Remove the item from the open set
+        openset.remove(current)
+        #Add it to the closed set
+        closedset.add(current)
+        #Loop through the node's children/siblings
+        for node in children(current,grid):
+            #If it is already in the closed set, skip it
+            if node in closedset:
+                continue
+            #Otherwise if it is already in the open set
+            if node in openset:
+                #Check if we beat the G score 
+                new_g = current.G + current.move_cost(node)
+                if node.G > new_g:
+                    #If so, update the node to have a new parent
+                    node.G = new_g
+                    node.parent = current
+            else:
+                #If it isn't in the open set, calculate the G and H score for the node
+                node.G = current.G + current.move_cost(node)
+                node.H = manhattan(node, goal)
+                #Set the parent to our current item
+                node.parent = current
+                #Add it to the set
+                openset.add(node)
 
 def Manhatthan(x, y, goalNode):
     return (abs(x - goalNode[0]) + abs(y - goalNode[1]))
