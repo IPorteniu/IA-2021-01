76d70282e9d235497b2b5619e47fb34d65de0e5e
Shiohne <Shiohne@github.com>
2021-04-05 10:01:11 -0500

    findSol from main to func

--- main.py
+++ main.py
@@ -8,21 +8,24 @@ from GridUtils import Node
 lenght = 40
 width = 40 
 
-#Se asigna la meta y el inicio
+def findSol(graph):
+    n = graph.findPath()
+    sol = []
+    while True:
+        sol.append(n.position)
+        if n.parentNode == None:
+            break
+        n = n.parentNode
+    sol.reverse()
+    print(list(sol))
 
+
+#Se asigna la meta y el inicio
 goal = (30,30)
 start = (0,0)
 graph = Grid()
 graph.buildGrid(lenght, width,start, goal)
-n = graph.findPath()
-sol = []
-while True:
-    sol.append(n.position)
-    if n.parentNode == None:
-        break
-    n = n.parentNode
-sol.reverse()
-print(list(sol))
+findSol(graph)
 
 
 
